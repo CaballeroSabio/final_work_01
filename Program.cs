42e328cc14c31a85f04039ba17ddfc49960c6144
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-10-04 16:28:03 +0300

    created the new code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+void Print(int n, int res)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {n} = {res}");
+}
+
+int result = GetSum(num);
+Print(num, result);
+
+Console.WriteLine();
